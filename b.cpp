e21e8dff48d0da8a7c16eb671f94fd3cd3faacb4
Wooden-Flute <1258963385@qq.com>
2025-09-02 20:24:01 +0800

    Update b.cpp

--- b.cpp
+++ b.cpp
@@ -4,5 +4,6 @@ int main() {
     cout << "Hello, World!" << endl;
     cout << "This project uses the C++17 language standard version." << endl;
     cout << "This file is located at: Testpy/b.cpp" << endl;
+    cout << "这是在云端更新的" << endl;
     return 0;
 }
